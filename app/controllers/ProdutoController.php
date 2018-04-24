270a43218646a942697e6a86304f756caf0cea0b
Felipe <felipinho_finaze@hotmail.com>
2018-04-23 22:10:54 -0300

    View Produto

---
+++ app/controllers/ProdutoController.php
@@ -0,0 +1,24 @@
+<?php
+class ProdutoController extends \Phalcon\Mvc\Controller
+{
+    public function cadastrarAction()
+    {
+        $clienteInsert = new Cliente();
+
+        $clienteInsert->id_cliente = 2;
+        $clienteInsert->nome = "Joao";
+        $clienteInsert->remover();
+
+        foreach($clienteInsert->buscar("") as $item)
+        {
+            echo $item->nome;    
+            echo "<br>";
+        }
+    }
+
+    public function novoAction()
+    {
+        return $this->view->pick('produto');
+    }
+}
+?>
