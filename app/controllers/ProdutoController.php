aee7bc6614af0148d50a397030a8b9c8272b0c75
Wesley Costa <wesley_costa@outlook.com>
2018-04-25 19:52:00 -0300

    Organização estrutura de arquivos

--- app/controllers/ProdutoController.php
+++ app/controllers/ProdutoController.php
@@ -2,7 +2,9 @@
 
 class ProdutoController extends \Phalcon\Mvc\Controller
 {
-
-    
+    public function inserirAction()
+    {
+        $this->view->pick('produto/inserir');
+    }
 }
 ?>
--- app/controllers/clienteController.php
+++ app/controllers/clienteController.php
@@ -1,19 +1,58 @@
 <?php
 class ClienteController extends \Phalcon\Mvc\Controller
 {
-    public function cadastrarAction()
+
+    public function indexAction()
+    {
+        $cliente = new Cliente();
+        $pesquisa = '';
+
+        if(isset($this->request))
+            $pesquisa = $this->request->getPost('pesquisa');
+        
+        $resultado = $cliente->buscar($pesquisa);
+
+        $this->view->setVar('resultado', $resultado);
+        $this->view->pick('cliente/inicial');
+    }
+
+
+    public function inserirAction()
+    {
+        $this->view->pick('cliente/inserir');
+    }
+
+    public function inserirDadosAction()
     {
         $clienteInsert = new Cliente();
 
-        $clienteInsert->id_cliente = 2;
-        $clienteInsert->nome = "Joao";
-        $clienteInsert->remover();
+        $clienteInsert->nome        = $this->request->getPost('nome');
+        $clienteInsert->cpf         = $this->request->getPost('cpf');
+        $clienteInsert->celular     = $this->request->getPost('celular');
+        $clienteInsert->telefone    = $this->request->getPost('telefone');
+        $clienteInsert->email       = $this->request->getPost('email');
+     
+        $clienteInsert->inserir();
+    }
 
-        foreach($clienteInsert->buscar("") as $item)
-        {
-            echo $item->nome;    
-            echo "<br>";
-        }
+    public function editarAction()
+    {
+        $this->view->pick('cliente/editar');
+    }
+
+    public function editarDadosAction()
+    {
+
+    }
+
+    public function removerAction()
+    {
+        $this->view->pick('cliente/remover');
+    }
+
+    public function removerDadosAction()
+    {
+
     }
 }
 ?>
